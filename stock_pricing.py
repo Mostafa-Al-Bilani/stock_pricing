e543c949a787056836db39a877217cd0471a9711
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2024-11-14 13:40:39 +0200

    cleaned the code

--- stock_pricing.py
+++ stock_pricing.py
@@ -1,14 +1,17 @@
 """A function that returns max profit achievable"""
-prices = [1, 2, 8, 10, 20, 26] # a list of stock prices where the index represents the days 
+prices = [1, 2, 8, 10, 20, 26] # a list of stock prices where the index represents the days
 LENGTH = len(prices)
 def max_profit(prices):
-    new_profit = 0
+    maximum_profit = 0
+#in this for loop we choose the day to buy on
     for i in range(LENGTH):
-        buy = prices[i]            
-        for j in range (i+1, LENGTH):
+        buy = prices[i]
+#in this for loop we choose the day to sell on and then compare the profit that can be made
+        for j in range (i + 1, LENGTH):
             sell = prices[j]
             profit = sell - buy
-            if  profit > new_profit:
-                new_profit = profit
-    return new_profit
+            if  profit > maximum_profit:
+                maximum_profit = profit
+
+    return maximum_profit
 print(max_profit(prices))
