6a8848dbd34eba53602a9fc54e47daf6cb1dd5b4
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2024-11-14 13:31:32 +0200

    declared the variables

---
+++ stock_pricing.py
@@ -0,0 +1,4 @@
+"""A function that returns max profit achievable"""
+prices = [1, 2, 8, 10, 20, 26] # a list of stock prices where the index represents the days 
+LENGTH = len(prices)
+def max_profit(prices):
