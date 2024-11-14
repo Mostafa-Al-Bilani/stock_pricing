8c3b0e8579c6e4a305ec18eb355efa01f31c5813
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2024-11-14 13:33:22 +0200

    created the function

--- stock_pricing.py
+++ stock_pricing.py
@@ -2,3 +2,13 @@
 prices = [1, 2, 8, 10, 20, 26] # a list of stock prices where the index represents the days 
 LENGTH = len(prices)
 def max_profit(prices):
+    new_profit = 0
+    for i in range(LENGTH):
+        buy = prices[i]            
+        for j in range (i+1, LENGTH):
+            sell = prices[j]
+            profit = sell - buy
+            if  profit > new_profit:
+                new_profit = profit
+    return new_profit
+print(max_profit(prices))
